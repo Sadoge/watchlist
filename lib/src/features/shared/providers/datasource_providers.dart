8e78bcbfe69bd41b5a4e87a2637c175eb877368d
Sam Smets <samsmets@hotmail.com>
2024-07-24 15:54:29 +0800

    Make api key save

--- lib/src/features/shared/providers/datasource_providers.dart
+++ lib/src/features/shared/providers/datasource_providers.dart
@@ -6,7 +6,11 @@ import 'package:watchlist/src/features/shared/data/models/tv_series_model.dart';
 import 'package:http/http.dart' as http;
 
 final httpProvider = Provider((ref) => http.Client());
-final tmdbDataSourceProvider = Provider((ref) =>
-    TMDBDataSource(ref.read(httpProvider), 'd57b258511eadb6e87fd312cefa6fa96'));
+
+final tmdbDataSourceProvider = Provider(
+  (ref) => TMDBDataSource(
+      ref.read(httpProvider), const String.fromEnvironment('TMDB_API_KEY')),
+);
+
 final hiveDataSourceProvider =
     Provider((ref) => HiveDataSource(Hive.box<TVSeriesModel>('tvSeriesBox')));
